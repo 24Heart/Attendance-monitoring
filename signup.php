4ce31f11432d5ed9ed5e4e97691c0c63f53faf33
24Heart <103588027+24Heart@users.noreply.github.com>
2023-12-10 21:59:10 +0800

    first commit

---
+++ signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Signup</title>
+  <!-- Your CSS and other header includes -->
+</head>
+<body class="bg-dark">
+  <main id="main">
+    <div class="align-self-center w-100">
+      <h4 class="text-white text-center">Signup</h4>
+      <div id="signup-center" class="bg-dark row justify-content-center">
+        <div class="card col-md-4">
+          <div class="card-body">
+            <form id="signup-form">
+              <div class="form-group">
+                <label for="username" class="control-label">Username</label>
+                <input type="text" id="username" name="username" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="password" class="control-label">Password</label>
+                <input type="password" id="password" name="password" class="form-control">
+              </div>
+              <!-- Add other required fields for signup -->
+              <center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Signup</button></center>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- Your scripts and other footer includes -->
+</body>
+</html>
